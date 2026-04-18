8c24783185cb82b46c0535be56ed015c1b263e64
SHNWAZ DEV <shnwazdeveloperx@gmail.com>
2026-04-18 22:57:01 +0530

    Update songs_handler.py

--- jiosaavn/plugins/songs_handler.py
+++ jiosaavn/plugins/songs_handler.py
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 async def handle_song_callback(client: Bot, callback: CallbackQuery):
     msg = callback.message
     await callback.answer()
-    
+
     data = callback.data.split("#")
     song_id = data[1]
     item_id, search_type, back_type = (None, "songs", None)
@@ -31,22 +31,22 @@ async def handle_song_callback(client: Bot, callback: CallbackQuery):
     try:
         response = await Jiosaavn().get_song(song_id=song_id)
         if not response or not response.get("songs"):
-            return await msg.edit("**The requested song could not be found.**")
+            return await msg.edit("**ᴛʜᴇ ʀᴇQᴜᴇsᴛᴇᴅ sᴏɴɢ ᴄᴏᴜʟᴅ ɴᴏᴛ ʙᴇ ғᴏᴜɴᴅ.**")
     except RuntimeError as e:
         logger.error(e)
         traceback.print_exc()
-        return await msg.edit("Connection refused by jiosaavn api. Please try again")
+        return await msg.edit("ᴄᴏɴɴᴇᴄᴛɪᴏɴ ʀᴇғᴜsᴇᴅ ʙʏ ᴊɪᴏsᴀᴀᴠɴ ᴀᴘɪ. ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ")
 
     song_data = response["songs"][0]
 
-    title = song_data.get("title", "Unknown")
+    title = song_data.get("title", "ᴜɴᴋɴᴏᴡɴ")
     title = html.unescape(title)
     formatted_title = title.replace(" ", "-")
-    language = song_data.get("language", "Unknown")
+    language = song_data.get("language", "ᴜɴᴋɴᴏᴡɴ")
     play_count = song_data.get("play_count", "0")
     play_count = int(play_count) if play_count else 0
     more_info = song_data.get("more_info", {})
-    album = more_info.get("album", "Unknown")
+    album = more_info.get("album", "ᴜɴᴋɴᴏᴡɴ")
     artist_map = more_info.get("artistMap", {})
     artists = artist_map.get("artists", [])
 
@@ -65,16 +65,16 @@ async def handle_song_callback(client: Bot, callback: CallbackQuery):
 
     text_data = [
         f"[\u2063]({image_url})"
-        f"**🎧 Song:** [{title}]({song_url})" if title else '',
-        f"**📚 Album:** [{album}]({album_url})" if album else '',
-        f"**🎵 Music:** {music}" if music else '',
-        f"**▶️ Plays:** {play_count:,}" if play_count else '',
-        f"**👨‍🎤 Singers:** {singers}" if singers else '',
-        f"**✍️ Lyricist:** {lyricists}" if lyricists else '',
-        f"**👫 Actors:** {actors}" if actors else '',
-        f"**📰 Language:** {language}" if language else '',
-        f"**📆 Release Date:** __{release_date}__" if release_date else '',
-        f"**📆 Release Year:** __{release_year}__" if not release_date and release_year else '',
+        f"**sᴏɴɢ:** [{title}]({song_url})" if title else '',
+        f"**ᴀʟʙᴜᴍ:** [{album}]({album_url})" if album else '',
+        f"**ᴍᴜsɪᴄ:** {music}" if music else '',
+        f"**ᴘʟᴀʏs:** {play_count:,}" if play_count else '',
+        f"**sɪɴɢᴇʀs:** {singers}" if singers else '',
+        f"**ʟʏʀɪᴄɪsᴛ:** {lyricists}" if lyricists else '',
+        f"**ᴀᴄᴛᴏʀs:** {actors}" if actors else '',
+        f"**ʟᴀɴɢᴜᴀɢᴇ:** {language}" if language else '',
+        f"**ʀᴇʟᴇᴀsᴇ ᴅᴀᴛᴇ:** __{release_date}__" if release_date else '',
+        f"**ʀᴇʟᴇᴀsᴇ ʏᴇᴀʀ:** __{release_year}__" if not release_date and release_year else '',
     ]
     text = "\n\n".join(filter(None, text_data))
 
@@ -86,24 +86,24 @@ async def handle_song_callback(client: Bot, callback: CallbackQuery):
         back_button_callback_data = f"search#{search_type}"
 
     buttons = [[
-        InlineKeyboardButton('Upload to TG 📤', callback_data=f'upload#{song_id}#song')
+        InlineKeyboardButton('ᴜᴘʟᴏᴀᴅ ᴛᴏ ᴛɢ', callback_data=f'upload#{song_id}#song')
     ], [
-        InlineKeyboardButton('🔙', callback_data=back_button_callback_data)
+        InlineKeyboardButton('ʙᴀᴄᴋ', callback_data=back_button_callback_data)
     ], [
-        InlineKeyboardButton('Close ❌', callback_data="close")
+        InlineKeyboardButton('ᴄʟᴏsᴇ', callback_data="close")
     ]]
+
     if more_info.get('has_lyrics') == 'true':
         lyrics_id = song_data.get("id")
         lyrics_button_callback_data = f"lyrics#{lyrics_id}#{song_id}#{search_type}"
         if item_id:
             lyrics_button_callback_data += f"#{item_id}#{back_type}"
-
-        buttons[0].insert(0, InlineKeyboardButton("Lyrics 📃", callback_data=lyrics_button_callback_data))
+        buttons[0].insert(0, InlineKeyboardButton("ʟʏʀɪᴄs", callback_data=lyrics_button_callback_data))
 
     await msg.edit(text=text[:4096], reply_markup=InlineKeyboardMarkup(buttons))
-    
-    # required to create some traffic to koyeb to keep it running
+
     await Jiosaavn()._request_data(url=f"http://{HOST}:{PORT}")
+
 
 @Bot.on_callback_query(filters.regex(r"^lyrics#"))
 async def lyrics(client: Bot, callback: CallbackQuery):
@@ -111,25 +111,25 @@ async def lyrics(client: Bot, callback: CallbackQuery):
     lyrics_id = data[1]
 
     response = await Jiosaavn().get_song_lyrics(lyrics_id=lyrics_id)
-    lyrics = response.get("lyrics", "")
-    lyrics = lyrics.replace("<br>", "\n")
-    if not lyrics:
-        await callback.answer("**The requested song could not be found.**", show_alert=True)
+    lyrics_text = response.get("lyrics", "")
+    lyrics_text = lyrics_text.replace("<br>", "\n")
+    if not lyrics_text:
+        await callback.answer("ᴛʜᴇ ʀᴇQᴜᴇsᴛᴇᴅ sᴏɴɢ ᴄᴏᴜʟᴅ ɴᴏᴛ ʙᴇ ғᴏᴜɴᴅ.", show_alert=True)
         return
 
-    if len(lyrics) <= 4096:
+    if len(lyrics_text) <= 4096:
         callback_data = "song#" + "#".join(data[2:])
-        button = [[InlineKeyboardButton('🔙', callback_data=callback_data)]]
+        button = [[InlineKeyboardButton('ʙᴀᴄᴋ', callback_data=callback_data)]]
         try:
             await callback.answer()
-            await callback.message.edit(lyrics, reply_markup=InlineKeyboardMarkup(button))
+            await callback.message.edit(lyrics_text, reply_markup=InlineKeyboardMarkup(button))
         except:
             pass
     else:
-        await callback.answer("Sending a song lyrics document")
+        await callback.answer("sᴇɴᴅɪɴɢ ᴀ sᴏɴɢ ʟʏʀɪᴄs ᴅᴏᴄᴜᴍᴇɴᴛ")
         file_location = f'{response.get("snippet")} song lyrics.txt'
         with open(file_location, 'w') as f:
-            f.write(lyrics)
+            f.write(lyrics_text)
 
         await client.send_document(
             chat_id=callback.from_user.id,
